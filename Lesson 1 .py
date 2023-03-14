ef38485810f568d0be87dde1e576bf0cef94b800
mercuric_novel0 <65826267+Vfrcbv0701058@users.noreply.github.com>
2023-03-14 15:54:43 +0100

    Добавлен урок №3 а так же новый файл с практикой

--- Lesson 1 .py
+++ Lesson 1 .py
@@ -3,6 +3,6 @@ num_2 = int(input("Enter second num: "))
 res = num_1+num_2
 Res = input("Enter something ")
 Res *= 5
-res*=5
+res *= 5
 print("Result is ", res)
 print("Result is ", Res)
---
+++ Lesson 2.py
@@ -0,0 +1,14 @@
+num = int(input("Ведите текст "))
+
+if num > 0:
+    print("Hello\n")
+elif num < 0:
+    print("< 0")
+else:
+    print("num = 0")
+print("Hello 1")
+
+# Смена значения переменнй с помощью условного оператора
+name = input()
+A = 'Yes' if name != "test" else 'No'
+print(A)
