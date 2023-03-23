6cf629406e19c94701f99dd02f081a86bac85f96
mercuric_novel0 <65826267+Vfrcbv0701058@users.noreply.github.com>
2023-03-23 10:07:11 +0100

    Add folder with practice

---
+++ Practice/elem in seq.py
@@ -0,0 +1,11 @@
+def check(seq, elem):
+    # if elem in seq:
+    #     return True
+    # else:
+    #     return False
+    # или 
+    return elem in seq
+
+a=('roe12320re9dsn2')
+element = input("Enter element: ")
+print(check(a, element))
