9046dfec87bcf895c8c14d396d768c13410f8709
mercuric_novel0 <65826267+Vfrcbv0701058@users.noreply.github.com>
2023-04-10 19:20:22 +0200

    Add some new

--- Lesson 6.py
+++ Lesson 6.py
@@ -1,10 +1,19 @@
-l=[34,'ssd',56,34.34]
-# print (l[-2])
+word = "football, basketball, skate"
+print (len(word)) # возвращает кол-ство элементов в строке word
+print (word.count("m")) # возвращает кол-ство m в сроке word
+print (word.upper())
+print (word.isupper())
+hobby = word.split(", ")
+print (hobby[1])
 
-i=0
-while i<4:
-    print (l[i])
-    i+=1
+for i in range(len(hobby)):
+    hobby[i] = hobby[i].capitalize()
 
-print(l[::2])
-print(l[::-2])
+result = ", ". join(hobby)
+print (result)
+
+word = "Football"
+print(word[1:-1:2]) 
+
+lis = [6,4,6,"stroka", True, 6.5, False, "nfdiw"]
+print (lis [2:7])
